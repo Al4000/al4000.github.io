c4c3a0db968b554916a4158e05bc39740d1af371
Alexander <lame_saint@mail.ru>
2017-08-22 11:57:46 +0300

    php to html

--- moonshine/countryCheck.php
+++
@@ -1,6 +0,0 @@
-<?php
-  if($_GET['country'] == 1)
-    echo json_encode(array("1" => "Atlas", "2" => "Shrugged"));
-  else if ($_GET['country'] == 2)
-    echo json_encode(array("3" => "Brave", "4" => "New World"));
-?>
