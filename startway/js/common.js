6e7e7b9160e1c47f44d37ac89f086b59c41ab13d
Alexander <lame_saint@mail.ru>
2017-09-12 17:07:48 +0300

    js

--- startway/js/common.js
+++ startway/js/common.js
@@ -226,3 +226,43 @@ $("#develop").mouseleave(function(){
 	$(".desc_over_dev").css({'width' : '0'});
 	$(".about").css('zIndex', '2');
 });
+
+/* sorting */
+  var $container = $('.proj_wrap');
+
+   $container.isotope({
+          filter: '*',
+          itemSelector: '.proj',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+
+  $('#portfolio-filter ul li a').on('click',function(){
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+          filter: selector,
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+    return false;
+  });
+
+  var $optionSets = $('#portfolio-filter ul'),
+         $optionLinks = $optionSets.find('a');
+
+         $optionLinks.on('click',function(){
+            var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+            return false;
+        }
+     var $optionSet = $this.parents('#portfolio-filter ul');
+     $optionSet.find('.selected').removeClass('selected');
+     $this.addClass('selected');
+  });
