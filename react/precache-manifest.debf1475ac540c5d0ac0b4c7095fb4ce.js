d882e035462657198633773bd86bc2db42f91db2
Alexander <kazakov_ai87@mail.ru>
2019-04-24 23:33:18 +0300

    prod

---
+++ react/precache-manifest.debf1475ac540c5d0ac0b4c7095fb4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5785e5789658a5b8005",
+    "url": "/react/static/css/main.d8fe4d0b.chunk.css"
+  },
+  {
+    "revision": "a5785e5789658a5b8005",
+    "url": "/react/static/js/main.cb2f20d4.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "632b72ab40a4e357f0b9",
+    "url": "/react/static/css/2.a5fc09e6.chunk.css"
+  },
+  {
+    "revision": "632b72ab40a4e357f0b9",
+    "url": "/react/static/js/2.538c7386.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "1401bbe9d86c447f17a70069f8107dd8",
+    "url": "/react/index.html"
+  }
+];
